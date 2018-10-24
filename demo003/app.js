c143c16f3ffd384f3dafe6ab3fad851aa37eb90d
Administrator <jieke_rose@foxmail.com>
2018-10-25 00:44:53 +0800

    三、路由中间件 koa-router 请求参数获取

--- demo003/app.js
+++ demo003/app.js
@@ -1,18 +1,24 @@
 const Koa = require('koa')
+const path = require('path')
 const registerRouter  = require('./routes')
-const bodyParser = require('koa-bodyparser');
+// const bodyParser = require('koa-bodyparser');
 const koaBody = require('koa-body');
+const static = require('koa-static');
+
 const app = new Koa()
 
-app.use(bodyParser());
+
 app.use(registerRouter())
+app.use(static(
+    path.join( __dirname, 'public')
+))
 app.use(koaBody({
     multipart: true,
     formidable: {
-        maxFileSize: 200*1024*1024 // 设置上传文件大小最大限制，默认2M
+        maxFileSize: 2000*1024*1024 // 设置上传文件大小最大限制，默认2M
     }
 }));
-
+// app.use(bodyParser());
 
 app.listen(8080)
 console.log("demo in run")
--- demo003/controller/user.js
+++ demo003/controller/user.js
@@ -1,10 +1,11 @@
 const Router = require('koa-router')
 const path = require('path')
 const router = new Router()
-var fs = require('fs')
+const fs = require('fs')
+const koaBody = require('koa-body');
 
 let prefix = path.basename(__filename)
-router.prefix('/' + prefix.substring(0, prefix.indexOf('.')))
+router.prefix('/api/' + prefix.substring(0, prefix.indexOf('.')))
 
 // 获取get方式的传值
 router.get('/',(ctx, next)=>{
@@ -13,7 +14,7 @@ router.get('/',(ctx, next)=>{
     ctx.body = "query=" + JSON.stringify(query);
 })
 //获取动态路由参数
-router.get('/user/:id/:username',(ctx, next)=>{
+router.get('/query/:id/:username',(ctx, next)=>{
     let query =  ctx.params;
     console.log(query);
     ctx.body = "query=" + JSON.stringify(query);
@@ -21,21 +22,22 @@ router.get('/user/:id/:username',(ctx, next)=>{
 
 // 获取post提交的参数，键值对方式
 router.post('/add',(ctx, next)=>{
-    let query =  ctx.request.body;
-    console.log(query);
-    ctx.body = "query=" + JSON.stringify(query);
+    let query =  ctx;
+    console.log(query.req);
+    ctx.body = 'query' + JSON.stringify(ctx.request.body);
 })
 
 // 获取post提交的参数，json字符串方式
-router.post('/add',(ctx, next)=>{
+router.post('/adddd',(ctx, next)=>{
     let query =  ctx.request.body;
     console.log(query);
     ctx.body = "query=" + JSON.stringify(query);
 })
 
 // 上传单个文件
-router.post('/upload',(ctx, next)=>{
+router.post('/upload', koaBody(),(ctx, next)=>{
     const file = ctx.request.files; // 获取上传文件
+    console.log(file)
     // 创建可读流
     const reader = fs.createReadStream(file.path);
     let filePath = path.join(__dirname, 'public/upload/') + `/${file.name}`;
